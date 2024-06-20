027488506c56a2864eba55f4331b55723c270881
ToyB-Chan <jasjason0otto@gmail.com>
2024-06-21 01:45:22 +0200

    Update main.c

--- main.c
+++ main.c
@@ -8,15 +8,13 @@ int main()
 {
 	ShellInfo* shell = ShellInfo_New();
 
-	/*
 	// Set the input stream to no blocking
 	int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
 	assert(flags >= 0);
 	int ret = fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
 	assert(ret >= 0);
-	*/
 	
-	// Set the terminal to non-canonical (disables input processing by the terminal, which also blocks)
+	// Set the terminal to non-canonical (disables input processing by the terminal, which also blocks the input stream)
 	struct termios term;
 	int ret;
 	ret = tcgetattr(STDIN_FILENO, &term);
