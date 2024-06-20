f8eeb87e387e73c3b0fc7715e3afe02a82469816
ToyB-Chan <jasjason0otto@gmail.com>
2024-06-20 04:06:18 +0200

    Update test_wait.c

--- test_wait.c
+++ test_wait.c
@@ -1,18 +1,38 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdbool.h>
 
-int main()
+bool SafeAtoi(const char* str, int* outInt)
 {
-	printf("I'll wait!\n");
-	fflush(stdout);
-	for (int i = 0; i < 3; i++)
+	int n = atoi(str);
+	if (n == 0 && str[0] != '0')
+		return false;
+	
+	*outInt = n;
+	return true;
+}
+
+
+int main(int argc, char** argv)
+{	
+	int seconds = 3;
+	if(argc < 2)
+	{
+		printf("No Argument given, defauling to 3 seconds\n");	
+	}
+	else if (!SafeAtoi(seconds, &seconds))
+	{
+		printf("Invalid argument given, exiting...\n");
+		return 1;
+	}
+
+	printf("I'll wait for %i seconds!\n", seconds);
+	for (int i = 0; i < seconds; i++)
 	{
 		sleep(1);
 		printf("Sleeping since %i seconds\n", i + 1);
-		fflush(stdout);
 	}
 
 	printf("Done!\n");
-	fflush(stdout);
 	return 0;
 }
