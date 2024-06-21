2d901a07014de413a032572982ad063d6f18cf12
ToyB-Chan <jasjason0otto@gmail.com>
2024-06-21 03:39:08 +0200

    Update test_input.c

--- test_input.c
+++ test_input.c
@@ -6,7 +6,7 @@ int main(int argc, char** argv)
 	printf("I want some input: \n");
 
 	char buffer[1024];
-	read(stdin, buffer, sizeof(buffer) - 1);
+	fread(buffer, sizeof(char), sizeof(buffer) - 1, stdin);
 	buffer[sizeof(buffer) - 1] = '\0';
 
 	printf("Your input was: %s\n", buffer);
